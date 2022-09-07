2de9d2bec72bf90a29bd05f2f5167f5ec293d6ec
Jaymerist <76460057+Jaymerist@users.noreply.github.com>
2022-09-07 09:39:10 -0600

    Add files via upload

---
+++ WestCostSales/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+purpose: Find # of sales
+author: Mihiri Kamiss
+date: September 7, 2022
+*/
+namespace WestCostSales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare variables and constants
+            const double Percentage = 0.43; //const is short for constant
+            double total = 5300000,
+                   sales;
+
+            // calculate 43% of total sales
+            sales = Percentage * total;
+
+            // display results
+            Console.WriteLine($"The company earned {sales:c} on {total:c} sales"); //c stands for currency formatting 
+        }
+    }
+}
